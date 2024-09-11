e0ffe9c5076f197c8ae58215e2c72a1c8a1de122
Angel Febles <licenciadofebles@gmail.com>
2024-09-11 15:21:26 -0500

    Bug fix

--- housingCost.py
+++ housingCost.py
@@ -2,11 +2,9 @@ import requests
 
 APIKEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2IiwianRpIjoiN2EwOGVjZWQyOTc4NGUzNGE3YzNiNWU0YjgyZTEyMWJlYzk3MzZhMmM5YjExMDRjM2Q0NjQ1NDFiZjBmMTM4MDMzMGE3ZjMxNmNjYjNiZGQiLCJpYXQiOjE3MjU1NjA2OTUuOTY3NDUxLCJuYmYiOjE3MjU1NjA2OTUuOTY3NDUzLCJleHAiOjIwNDEwOTM0OTUuOTYyNjAzLCJzdWIiOiI3NzI3MyIsInNjb3BlcyI6W119.PC5tJhuUKPf0LDMADlgADvWzJR16y2xTWOi-S2pWUrBy25P3fMWQTabtuPe8xKfYzuxApEYv8wvVbBHHdPt8yQ'
 countyCode = '5510199999'  # Racine, WI MSA
-year = 2025
 
-#FY 2024 Racine, WI MSA FMTs for All Bedroom Sizes
-#Deleting year property defaults to 2025's predictions
-url = f'https://www.huduser.gov/hudapi/public/fmr/data/{countyCode}?year={year}'
+#FY Racine, WI MSA FMTs for All Bedroom Sizes
+url = f'https://www.huduser.gov/hudapi/public/fmr/data/{countyCode}'
 
 headers = {
     'Authorization': f'Bearer {APIKEY}'
