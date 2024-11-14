5d929eb5926ef5c432e566c4f59542ad63c6aa7a
Angel Febles <licenciadofebles@gmail.com>
2024-11-14 15:58:26 -0600

    Improved documentation

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from scripts.jobDataScrapeSEL import jobDataScrapeStarter
 """
 This is the starter script for the project.
 It calls all the other scripts and outputs the data to an excel file. -> dataOutput.xlsx
+It also outputs an excel file with job data (salaries, educational requirements, ...) -> jobData.xlsx
 
 dataOutput.xlsx contains the following sheets:
 
@@ -21,18 +22,35 @@ Housing_lookup  --- Costs for each housing plan
 Self_Sufficiency_Standard
 Food_lookup --- Costs for each food plan, divided by age and gender
 Food_means --- Average costs for each food plan, divided by age group (Adult, Infant, Preschooler, School Age, Teenager)
-
-
 Monthly Budget --- Addition of all the previous costs to determine the monthly budget according to family size
+
+
+jobData.xlsx contains the following sheets:
+
+Job_Data --- Misc data for each job (annual wage, hourly wage, number of jobs available, ...)
+Education_Requirements --- Educational requirements for each job
 
 """
 
 
-#Change these for your desired county
-countyCode_HousingCost = '5510199999'  # Racine, WI MSA
+#TODO: Change these for your desired county
 
+# For the SSS, this is the FIPS code of the county followed by five 9s.
+# You can get the FIPS code from here:
+# https://dpi.wi.gov/sfs/statistical/basic-facts/wisconsin-counties
+# So for example, Sauk County has has a FIPS code of 55111, so you would put 5511199999
+# Racine has a FIPS code of 55101, so you would put 5510199999, etc
+countyCode_HousingCost = '5510199999'  
+
+# Make sure to put county name with the first letter beging a capital letter 
+# Followed by the word "County" (wich also needs to have leading capital letter )
+# For example: "Racine County", "Dodge County", "Eau Claire County"
 county_SelfSufficiencyStandard = 'Racine County'
 
+
+#Make sure to put county name with the first letter beging a capital letter
+#Then, followed by the string ", WI" (For Wisconsin) (there is no space between county name and the comma)
+#For example: "Racine, WI", "Dodge, WI", "Eau Claire, WI"
 county_JobData = 'Racine, WI'
 
 
